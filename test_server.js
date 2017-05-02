f7012df004cff6fd6912ebc6beec06ea0e9f6ffb
Hack Reactor Student <student@hackreactor.com>
2017-05-02 16:03:10 -0700

    change res.send to JTROCKS

--- test_server.js
+++ test_server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response) {
-  response.send("HELLO WORLD!");
+  response.send("JT ROCKS!!!");
 });
 
 var port = process.env.PORT || 3000;
