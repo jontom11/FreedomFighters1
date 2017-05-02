948ef2d1e64bb0cca430884f4d2dec775088836f
Jonathan Tom <Jonathan.Y.Tom1@gmail.com>
2017-05-02 16:28:41 -0700

    Applied Server side PDF parse

--- test_server.js
+++ test_server.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var app = express();
+var pdf_table_extractor = require("pdf-table-extractor");
+
 
 app.get('/', function(request, response) {
-  response.send("HELLO WORLD!");
+  
+  function success(result) {
+    Result = JSON.parse( JSON.stringify(result) );
+    // console.log(  Result["pageTables"][0].tables );
+    Results.forEach((page)=>{
+
+	    response.send( Result["pageTables"][page].tables );
+    })
+  }
+  function error(err) {
+    console.error('Error: ' + err);
+  }
+ 
+  pdf_table_extractor("cat1_dist.pdf",success,error);
+ 
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 
 app.listen(port, function() {
   console.log(`Example app listening on ${port}`);
